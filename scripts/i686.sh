238c68dabc413f84273619dd3032cd972c8ebab7
rain1 <rain1@airmail.cc>
2018-09-29 20:56:45 +0100

    fix scripst

--- scripts/i686.sh
+++ scripts/i686.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -ex
 
-source ./scripts/i686-source.sh
-source ./scripts/build-generic.sh
 source ./versions.sh
 export VERSION
 export PREFIX="$(pwd)/out"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 export MAKEFLAGS="-j $(nproc --all)"
+source ./scripts/build-generic.sh
 
 build_binutils
 build_gcc
--- scripts/x64.sh
+++ scripts/x64.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -ex
 
-source ./scripts/x64-source.sh
-source ./scripts/build-generic.sh
 source ./versions.sh
 export VERSION
 export PREFIX="$(pwd)/out"
 export TARGET=x86_64-elf
 export PATH="$PREFIX/bin:$PATH"
 export MAKEFLAGS="-j $(nproc --all)"
+source ./scripts/build-generic.sh
 
 build_binutils
 build_gcc
